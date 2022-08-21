51700ec78c99ca20aeededd5c60e0dbdbda512e2
LeeHyunJin <tkghlwhfu@naver.com>
2022-08-21 11:52:36 +0900

    57강 글 삭제하기

--- src/main/java/com/hyunjin/blog/controller/boardController.java
+++ src/main/java/com/hyunjin/blog/controller/boardController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.hyunjin.blog.service.BoardService;
 
@@ -45,6 +46,14 @@ public class BoardController {
 	    model.addAttribute("boards", boardService.글목록(pageable));
 	    return "index";         //viewResolver 작동
 	  }
+	
+	
+	@GetMapping("/board/{id}")
+	public String findById(@PathVariable int id, Model model) {
+		model.addAttribute("board", boardService.글상세보기(id));
+		return "board/detail";
+	}
+	
 	
 	//권한이 필요
 	@GetMapping("/board/saveForm")
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,9 @@ let index = {
 		$("#btn-save").on("click", ()=>{	//functiin(){}, ()=> this를 바인딩 하기 위해서
 			this.save();
 		});
+		$("#btn-delete").on("click", ()=>{	//functiin(){}, ()=> this를 바인딩 하기 위해서
+			this.deleteById();
+		});
 	},
 	
 	save: function(){
@@ -19,6 +22,25 @@ let index = {
 			,	dataType: "json"	// 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 데이터로 변경을 해준다.
 		}).done(function(resp){
 			alert("글쓰기가 완료 되었습니다");
+			location.href = "/";
+		}).fail(function(error){
+			alert(JSON.strigify(error));
+		});
+
+	},
+	
+	deleteById: function(){
+		
+		alert("여기오냐");
+		var id = $("#id").text();
+		
+		$.ajax({
+				type:"DELETE"
+			, 	url:"/api/board/"+id
+			,	contentType:"application/json; charset = utf-8"
+			,	dataType: "json"	// 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 데이터로 변경을 해준다.
+		}).done(function(resp){
+			alert("삭제가 완료 되었습니다");
 			location.href = "/";
 		}).fail(function(error){
 			alert(JSON.strigify(error));
